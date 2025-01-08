ea3e8e33ccb3a2cc2dbc7181819ebd9f3cda6eb1
Alejandro Arco <alejandroarpu@gmail.com>
2025-01-08 17:12:40 +0100

    feat: add dialog name and user info

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import "./styles.css";
 function ScreenController() {
-  const arrowLeftButton = document.querySelector(".arrow-left-button");
-  function closeLateralMenu() {
-    document.querySelector(".lateral-menu").classList.toggle("closed");
-    document.querySelector(".arrow-left-button").classList.toggle("rotate");
-  }
-  arrowLeftButton.addEventListener("click", closeLateralMenu);
+  const showLateralMenu = () => {
+    const arrowLeftButton = document.querySelector(".arrow-left-button");
+
+    function toggleLateralMenu() {
+      document.querySelector(".lateral-menu").classList.toggle("closed");
+      document.querySelector(".arrow-left-button").classList.toggle("rotate");
+    }
+    arrowLeftButton.addEventListener("click", toggleLateralMenu);
+  };
+  const showNameDialog = () => {
+    const 
+  };
+  showNameDialog();
+  showLateralMenu();
 }
 
 ScreenController();
